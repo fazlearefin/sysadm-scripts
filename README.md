690baf15cb71af795b47c337ee5d890ee01890ab
Fazle Arefin <Fazle.Arefin@sportsbet.com.au>
2014-10-07 20:24:09 +1100

    Added hostid.sh and updated relevent info in README.md

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ sysadm-tools
 ============
 
 Collection of System Admin's tools for those things which still needs to be done manually
+
+* hostid.sh
+
+#hostid.sh
+This script lets you create your hostid of your choice on the host. You can either provide the hostid you want or you can choose to write the current hostid to `/etc/hosts`. Writing the hostid to `/etc/hosts` makes sure your hosid remains the same, say, even when you change your ip address.
+
+The `hostid` command lets you see the current hostid.
+
+
---
+++ hostid.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Purpose: Write the passed in parameter as hostid to /etc/hostid
+#          If no parameter is passed, write current hostid to /etc/hostid
+# Author:  Fazle Arefin
+
+if [ -n "$1" ]; then
+  host_id=$1
+  # chars must be 0-9, a-z, A-Z and exactly 8 chars
+  egrep -o '^[a-fA-F0-9]{8}$' <<< $host_id || exit 1
+else
+  host_id=$(hostid)
+fi
+
+a=${host_id:6:2}
+b=${host_id:4:2}
+c=${host_id:2:2}
+d=${host_id:0:2}
+
+echo -ne \\x$a\\x$b\\x$c\\x$d > /etc/hostid &&
+  echo "Success" 1>&2
+
+exit 0
